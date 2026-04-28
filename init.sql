db3dd3b24f208f1c970de357d762f95b541300b8
David Ricardo Nuñez Bernal <dnunezb@unal.edu.co>
2026-04-28 12:42:01 -0500

    Add files via upload

--- init.sql
+++ init.sql
@@ -1,10 +1,30 @@
+CREATE TABLE Sectors(
+    id_sector SERIAL PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL
+    industria VARCHAR(255) NOT NULL
+);
+CREATE TABLE Company(
+    symbol VARCHAR(10) NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    pais VARCHAR(255) NOT NULL,
+    id_sector INT NOT NULL,
+    PRIMARY KEY (symbol),
+    FOREIGN KEY (id_sector) REFERENCES Sectors(id_sector)
+);
+CREATE TABLE Leadership(
+    symbol VARCHAR(10) NOT NULL,
+    fecha DATE NOT NULL,
+    CEO VARCHAR(255) NOT NULL,
+    PRIMARY KEY (symbol, fecha),
+    FOREIGN KEY (symbol) REFERENCES Company(symbol)
+);
 CREATE TABLE Financial_Reports(
     id_reporte SERIAL PRIMARY KEY,
     symbol VARCHAR(10) NOT NULL,
     fecha DATE NOT NULL,
     Ingresos NUMERIC(15, 2) NOT NULL,
     Ganancias NUMERIC(15, 2) NOT NULL,
-    Activos NUMERIC(15, 2) NOT NULL,
+    Activos NUMERIC(15, 2) NOT NULL
 );
 CREATE TABLE Debt_Structure(
     id_deuda SERIAL PRIMARY KEY,
